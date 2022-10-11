720b518e96ea47411f61443c0ba710f3f73bcbdc
DeniJanko <denijanko@gmail.com>
2022-10-11 10:21:00 +0200

    zadatak3

--- 01-zadace/zadatak3.js
+++ 01-zadace/zadatak3.js
@@ -1,1 +1,10 @@
-gsgsg
+function broj(x) {
+    return (Math.abs(x>=100 && x<=150000)
+}
+
+if (x>=100 && x<=150000){
+cout<< x "je unutar [100, 150000]"
+}
+else{
+    cout<< x "nije unutar [100, 150000]"
+}
