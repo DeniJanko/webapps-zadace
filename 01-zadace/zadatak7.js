46fc078dc57afc212a9b50fb49822252feeb0756
DeniJanko <denijanko@gmail.com>
2022-10-11 10:57:35 +0200

    zadatak7

---
+++ 01-zadace/zadatak7.js
@@ -0,0 +1,13 @@
+function number(x){
+    return ((x=>0) && (x<=1000));
+}
+
+console.log(number(x))
+
+function number(x){
+    if (x%7==0){
+        return true;
+    } else {
+        return false;
+    }
+}
