d9a2e9dc3b7bd9066bf0a774a7a5c5db74f93450
DeniJanko <denijanko@gmail.com>
2022-10-11 10:11:58 +0200

    zadatak2

--- 01-zadace/zadatak2.js
+++ 01-zadace/zadatak2.js
@@ -1,2 +1,7 @@
-pravi File
-sfsf 2
+option value="HRK";
+option value="EUR";
+
+var convert = document.querySelector(".convert");
+var fromCurrecy = document.querySelector(".from");
+var toCurrecy = document.querySelector(".to");
+var finalValue = document.querySelector(".finalValue");
